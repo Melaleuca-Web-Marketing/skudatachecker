3faa3ecfa05b7e1193cdf6da9816f8007591a488
Jeff Torres <jtorres@melaleuca.com>
2025-11-10 13:34:25 -0700

    Update next config

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   basePath: '/skudatachecker',
-  // Silence that dev cross-origin warning when you open via your server hostname:
-  allowedDevOrigins: ['https://usifhqtsagrqt01.melaleuca.net'],
+  // you're on HTTP right now, so include http://
+  allowedDevOrigins: ['http://usifhqtsagrqt01.melaleuca.net'],
 };
 
 export default nextConfig;
